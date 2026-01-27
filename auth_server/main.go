8d751f187eeb3de98fe95c083df24199f614d9c3
Artur Kraev <artur.kraev@flant.com>
2026-01-27 22:43:28 +0300

    Add kubernetes auth logging
    Signed-off-by: Artur Kraev <artur.kraev@flant.com>

--- auth_server/main.go
+++ auth_server/main.go
@@ -58,7 +58,7 @@ func stringToUint16(s string) uint16 {
 }
 
 func ServeOnce(c *server.Config, cf string) (*server.AuthServer, *http.Server) {
-	glog.Infof("Config from %s (%d users, %d ACL static entries)", cf, len(c.Users), len(c.ACL))
+	glog.Infof("Config from %s (%d users, %d ACL static entries, kubernetes_auth: %t)", cf, len(c.Users), len(c.ACL), c.KubernetesAuth != nil)
 	as, err := server.NewAuthServer(c)
 	if err != nil {
 		glog.Exitf("Failed to create auth server: %s", err)
