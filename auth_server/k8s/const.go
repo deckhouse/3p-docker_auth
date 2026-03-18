076da8d9d69b1ffa6ec83ce14eae621f762e6314
Artur Kraev <artur.kraev@flant.com>
2026-03-18 22:52:27 +0300

    Revert "Update Kubernetes user labels for consistency"
    This reverts commit 60adcea2354d10459f3677aa6fa7e1409dd6b45b.

--- auth_server/k8s/const.go
+++ auth_server/k8s/const.go
@@ -17,8 +17,8 @@
 package k8s
 
 const (
-	UserLabel = "k8s-username"
-	UIDLabel  = "k8s-uid"
+	UserLabel = "username"
+	UIDLabel  = "uid"
 
 	// DefaultCacheSize is the default size for the LRU cache used in Kubernetes authorization.
 	// Ref: https://github.com/kubernetes/kubernetes/blob/release-1.31/staging/src/k8s.io/apiserver/plugin/pkg/authorizer/webhook/webhook.go#L130
